26f00ba64c8bb64f77d3d35bcb5b4d9661632221
vilksons <barokahsukawangi@gmail.com>
2025-03-06 10:10:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl -l -o pawnclis_win https://raw.githubusercontent.com/vilksons/pawnclis/refs/heads/main/Scripts/pawnclis_win
+curl -l -o pawnclis_win https://raw.githubusercontent.com/vilksons/pawnclis/refs/heads/main/Scripts/pawnclis
 
 bash
